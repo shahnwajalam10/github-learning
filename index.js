4ac5de9393f9f1fc99f897eb412c6d3886445256
shahnwaj alam <shahnwajalam10@gmail.com>
2024-10-06 13:55:30 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const age = 24;
-const name = "shahnawaz alam";
+const name1 = "shahnawaz alam";
 const dob = '10/04/2002';
 
 function sayMyName(){
-    console.log("My name is " + name);
+    console.log("My name is " + name1);
+}
+
+
+function isAbove18(){
+    return age >= 18
 }
